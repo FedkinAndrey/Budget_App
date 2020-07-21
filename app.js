5d091b6d9c435c87b34e4e71124732077bca7717
FedkinAndrey <fedkin17@gmail.com>
2020-07-21 23:59:28 +0300

    Adding new item to Budget Controller

--- app.js
+++ app.js
@@ -13,18 +13,48 @@ let budgetController = (function () {
         this.value = value
     }
 
-    var allExpenses = []
-    var allIncomes = []
-    var totalExpenses = 0
+    let allExpenses = []
+    let allIncomes = []
+    let totalExpenses = 0
 
-    var data = {
+    let data = {
         allItems: {
             exp: [],
             inc: []
         },
         totals: {
-            exp:0,
-            inc:0
+            exp: 0,
+            inc: 0
+        }
+    }
+
+    return {
+        addItem: function (type, des, val) {
+            let newItem, ID
+
+            //create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1
+            } else {
+                ID = 0
+            }
+
+            //create new item based on 'inc' or 'exp'
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val)
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val)
+            }
+
+            //push it into out data structure
+            data.allItems[type].push(newItem)
+
+            //return the new item
+            return newItem
+        },
+
+        testing: function () {
+            console.log(data)
         }
     }
 
@@ -75,10 +105,13 @@ let controller = (function (budgetCtrl, UICtrl) {
 
     let ctrlAddItem = function () {
 
+        let input, newItem
+
         // 1. Get the field input data
-        let input = UICtrl.getInput();
+        input = UICtrl.getInput();
 
         // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
 
@@ -91,6 +124,7 @@ let controller = (function (budgetCtrl, UICtrl) {
 
     return {
         init: function () {
+            console.log('app has started')
             setupEventListeners();
         }
     }
