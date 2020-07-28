cd4d36f555e69deb82810a793398f533ccbe5509
FedkinAndrey <fedkin17@gmail.com>
2020-07-28 22:21:46 +0300

    Clear input fields

--- app.js
+++ app.js
@@ -111,6 +111,24 @@ let UIController = (function () {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHTML)
         },
 
+
+        clearFields: function () {
+            let fields, fieldsArr
+
+            fields = document.querySelectorAll(DOMStrings.inputDescription + ', ' + DOMStrings.inputValue)
+
+            fieldsArr = Array.prototype.slice.call(fields)
+
+            fieldsArr.forEach(function (current, index, array) {
+                current.value = ""
+
+            })
+
+            fieldsArr[0].focus()
+
+        },
+
+
         getDOMStrings: function () {
             return DOMStrings;
         }
@@ -147,6 +165,10 @@ let controller = (function (budgetCtrl, UICtrl) {
 
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type)
+
+        // 4. Clear the fields
+        UICtrl.clearFields()
+
         // 4. Calculate the budget
 
         // 5.Display the budget on the UI
